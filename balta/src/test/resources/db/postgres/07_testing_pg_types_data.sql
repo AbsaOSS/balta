9d216c27f2b49817a94e80624f172f00ba229d8b
David Benedeki <14905969+benedeki@users.noreply.github.com>
2024-10-22 14:40:35 +0200

    #42 Add possibility to delete data from table (#45)
    * added methods `delete` to `DBTable` in several overloaded flavors
    * fixed `addNull` evaluation bug on `DBTable`
    * added license entries to testing SQLs
    * `DBTable` covered by integration tests
    
    ---------
    
    Co-authored-by: miroslavpojer <miroslav.pojer@absa.africa>

--- balta/src/test/resources/db/postgres/07_testing_pg_types_data.sql
+++ balta/src/test/resources/db/postgres/07_testing_pg_types_data.sql
@@ -1,5 +1,20 @@
-TRUNCATE testing.pg_types;
+/*
+ * Copyright 2023 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+TRUNCATE testing.pg_types;
 
 INSERT INTO testing.pg_types(
     id, json_type, jsonb_type, array_of_json_type)
